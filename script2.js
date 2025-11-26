7bf9622a6dcf00e7cacc5e098d23ebd530336dd1
MarcelaQuirino <marcelaquirinogs@hotmail.com>
2025-11-26 15:48:59 -0300

    coomit

--- script2.js
+++ script2.js
@@ -14,3 +14,29 @@ let meuNome = "Marcela Quirino";
     console.log("Minha idade é:");
 
     let saudacao = " Olá, meu nome é " + meuNOme + " e miha idade é " + minhaIdade +"!";
+
+
+//objeto
+    const hero = {
+        nome: 'Link',
+        vida: 100,
+        força: 20,
+    }
+
+    hero.vida = 90;
+    hero.arma = "espada";
+
+    console.log(`O nome do objeto é ${hero.nome}.`);
+    console.log(`A vida do objeto é ${hero.vida}.`);
+    console.log(`A força do objeto é ${hero.força}.`);
+
+
+    //Array
+    let fruits = ["maçã", "pera"]
+
+    console.log(fruits[0]);
+
+    fruits.push("uva");
+
+    console.log(fruits);
+
